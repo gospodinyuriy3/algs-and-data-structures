86378f700779c1bad55ec1e7dfa08fb426932488
Юра <korzhevsky.yura@yandex.ru>
2025-03-19 11:57:08 +0300

    left and right bin search and some changes

--- binary_search.py
+++ binary_search.py
@@ -1,17 +1,18 @@
-def binary_search(array, target) -> int:
+def binary_search(array, target) -> int | IndexError:
     if len(array) == 0:
         return IndexError
 
     left = 0
     right = len(array) - 1
 
-    
-    return binary_search_iterations(array, left, right, target)
-    return binary_search_recurse(array, left, right, target) #работает только если элемент 100% есть в массиве
+    res = binary_search_iterations(array, left, right, target)
 
-def binary_search_recurse(array, left, right, target) -> int:
+    return IndexError if not(res) else res
+    # return binary_search_recurse(array, left, right, target) работает только если элемент 100% есть в массиве
+
+def binary_search_recurse(array, left, right, target) -> int | bool:
     if left > right:
-        return None
+        return False
     
     mid = left + (right - left) // 2
 
@@ -24,9 +25,9 @@ def binary_search_recurse(array, left, right, target) -> int:
         return binary_search_recurse(array, array[mid] + 1, right, target)
 
 
-def binary_search_iterations(array, left, right, target) -> int:
+def binary_search_iterations(array, left, right, target) -> int | bool:
     if left > right:
-        return None
+        return False
     
     while left <= right:
         mid = left + (right - left) // 2
@@ -39,4 +40,36 @@ def binary_search_iterations(array, left, right, target) -> int:
         else:
             left = mid + 1
 
-    return None
+    return False
+
+def left_binary_search(array, left, right, target) -> int | bool:
+    while left + 1 < right:
+        mid = (right - left) // 2 + left
+
+        if target > array[mid]:
+            left = mid
+        else:
+            right = mid
+
+    if array[left] == target:
+        return left
+    if array[right] == target:
+        return right
+    
+    return False
+
+def right_binary_search(array, left, right, target) -> int | bool:
+    while left + 1 < right:
+        mid = (right - left) // 2 + left
+
+        if target < array[mid]:
+            right = mid
+        else:
+            left = mid
+
+    if array[right] == target:
+        return right
+    if array[left] == target:
+        return left
+    
+    return False
